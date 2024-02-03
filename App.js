e3982e099c43b3e4985ba1aecd60b545bd4192c4
martinromario55 <martinromario55@gmail.com>
2024-02-03 17:10:59 +0300

    Tab Navigation

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
-import 'react-native-gesture-handler'
-import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
-import DashboardScreen from './screens/DashboardScreen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import CourseListScreen from './screens/CourseListScreen'
+import ProfileScreen from './screens/ProfileScreen'
 import SettingsScreen from './screens/SettingsScreen'
 
-const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{
-            title: 'My Dashboard',
-            drawerLabel: 'Dashboard label',
-            drawerActiveTintColor: '#333',
-            drawerActiveBackgroundColor: 'lightblue',
-            drawerContentStyle: {
-              backgroundColor: '#c6cbef',
-            },
-          }}
-        />
-        <Drawer.Screen name="Settings" component={SettingsScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Course List" component={CourseListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
