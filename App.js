f8284e42b41a969254918c903d033306e62322ab
martinromario55 <martinromario55@gmail.com>
2024-02-03 17:27:18 +0300

    Nesting Navigators

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import CourseListScreen from './screens/CourseListScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import SettingsScreen from './screens/SettingsScreen'
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { AboutStack } from './AppStack'
 
 const Tab = createBottomTabNavigator()
 
@@ -43,6 +44,16 @@ export default function App() {
           options={{
             tabBarIcon: ({ color }) => (
               <Ionicons name="settings" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="About Stack"
+          component={AboutStack}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="information-circle" size={20} color={color} />
             ),
           }}
         />
