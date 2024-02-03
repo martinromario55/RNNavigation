abf72abecedf2599fd22900ef951d33a80ab5613
martinromario55 <martinromario55@gmail.com>
2024-02-03 17:19:55 +0300

    Tab Navigation Options

--- App.js
+++ App.js
@@ -3,16 +3,49 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import CourseListScreen from './screens/CourseListScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import SettingsScreen from './screens/SettingsScreen'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Course List" component={CourseListScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelPosition: 'below-icon',
+          tabBarShowLabel: true,
+          tabBarActiveTintColor: 'purple',
+        }}
+      >
+        <Tab.Screen
+          name="Course List"
+          component={CourseListScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="list" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'My Profile',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+            tabBarBadge: 1,
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="settings" size={20} color={color} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
